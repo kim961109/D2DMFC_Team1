29225f93195a167903b65a97b1d4365cd540232f
minsung6747 <90056087+minsung6747@users.noreply.github.com>
2022-05-31 18:38:06 +0900

    민성플레이어추가 씬 추가 오비제이 vLook추가

---
+++ 3rdTeamWork_Team1/3rdTeamWork_Team1/PlayerMS.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Obj.h"
+class CPlayerMS :
+	public CObj
+{
+public:
+	CPlayerMS();
+	virtual ~CPlayerMS();
+
+public:
+	virtual void Initialize(void) override;
+	virtual void Update(void) override;
+	virtual void Late_Update(void) override;
+	virtual void Render(HDC hDC) override;
+	virtual void Release(void) override;
+
+
+private:
+	void		Key_Input(void);
+
+private:
+	D3DXVECTOR3			m_vPoint[4];
+	D3DXVECTOR3			m_vOriginPoint[4];
+	D3DXVECTOR3			vLook;
+
+	float		m_fSpeed;
+	float		m_fAngle = 0.f;
+};
+
