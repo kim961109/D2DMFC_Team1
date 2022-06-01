21e4dcc1c2dcd194859dcde1543575413ee03e3b
DESKTOP-TMLO51S\kje29 <kje293@gmail.com>
2022-06-01 16:38:14 +0900

    TileMgr, Tile

--- 3rdTeamWork_Team1/3rdTeamWork_Team1/AbstractFactory.h
+++ 3rdTeamWork_Team1/3rdTeamWork_Team1/AbstractFactory.h
@@ -23,4 +23,12 @@ public:
 		return pObj;
 	}
 
+	static CObj*	Create(float _fX, float _fY,float _fZ)
+	{
+		CObj*	pObj = new T;
+
+		pObj->Set_Pos(_fX, _fY, _fZ);
+		pObj->Initialize();
+		return pObj;
+	}
 };
--- 3rdTeamWork_Team1/3rdTeamWork_Team1/Obj.h
+++ 3rdTeamWork_Team1/3rdTeamWork_Team1/Obj.h
@@ -9,7 +9,7 @@ public:
 
 public:
 	virtual void Initialize(void) PURE;
-	virtual void Update(void) PURE;
+	virtual int Update(void) PURE;
 	virtual void Late_Update(void) PURE;
 	virtual void Render(HDC hDC) PURE;
 	virtual void Release(void) PURE;
@@ -22,10 +22,19 @@ public:
 	RECT Get_Rect(void) { return m_tRect; }
 	INFO Get_Info(void) { return m_tInfo; }
 
+	void Set_Pos(float _fX, float _fY, float _fZ)
+	{
+		m_tInfo.vPos.x = _fX;
+		m_tInfo.vPos.y = _fY;
+		m_tInfo.vPos.z = _fZ;
+	}
+
 protected:
 	OBJINFO m_tObjInfo;//충돌하는 벽 같은 움직이지 않을 오브젝트는 이걸로 월드 좌표만 주고
 	RECT m_tRect;//움직이지 않는 물체의 렉트.
 
 	INFO m_tInfo;//움직이는 플레이어와 같은 오브젝트는 D3D 벡터 사용하자.
+
+	bool		m_bDead;
 };
 
